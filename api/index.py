8de0eedd7d79bc2154247341ab280cca8b0ba6c2
richard-polynia <richard@polynia.org>
2025-04-08 19:38:05 -0500

    minor changes

--- api/index.py
+++ api/index.py
@@ -48,6 +48,5 @@ class ChatBot(Resource):
 
 api.add_resource(ChatBot,"/chat")
 
-
-def handler(environ, start_response):
-    return app(environ, start_response)
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
